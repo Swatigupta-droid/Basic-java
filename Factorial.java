cc26877964d7a19b42fb85fb94ac252a1e1c3dc6
Ashish <55042068+reactash@users.noreply.github.com>
2021-10-03 21:23:33 +0530

    Create factorial.java

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+class Test 
+{ 
+    static int factorial(int n) 
+    { 
+        if (n == 0) 
+          return 1; 
+        return n*factorial(n-1); 
+    } 
+    
+    public static void main(String[] args)  
+    { 
+        int num = 5; 
+        System.out.println("Factorial of "+ num + " is " + factorial(5)); 
+    } 
+}  
