9f8777b4d0a0c6a4547ca96b971704708f19cacf
ThatOneMuffin <82111033+ThatOneMuffin@users.noreply.github.com>
2023-05-04 15:33:42 -0400

    Update BasicCalculator.java
    Added a menu
    Also added the quadratic formual calculator

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -1,75 +1,81 @@
-// Java program for simple calculator
-
-import java.io.*;
-import java.lang.*;
-import java.lang.Math;
 import java.util.Scanner;
-public class BasicCalculator {
 
-	public static void main(String[] args)
-	{
-		// stores two numbers
-		double num1, num2;
+public class BasicCalculator
+{
+public static void main(String args[])
+{
+    float a, b, res;
+    char select, ch;
+    Scanner scan = new Scanner(System.in);
 
-		// Take input from the user
-		Scanner sc = new Scanner(System.in);
+    do
+    {
+        System.out.print("(1) Addition\n");
+        System.out.print("(2) Subtraction\n");
+        System.out.print("(3) Multiplication\n");
+        System.out.print("(4) Division\n");
+	System.out.print("(5) Quadratic\n");
+        System.out.print("(6) Exit\n\n");
+        System.out.print("Enter Your Choice : ");
+        choice = scan.next().charAt(0);
 
-		System.out.println("Enter the numbers");
-
-		// take the inputs
-		num1 = sc.nextDouble();
-
-		num2 = sc.nextDouble();
-
-		System.out.println("Enter the operator (+,-,*,/)");
-
-		char op = sc.next().charAt(0);
-
-		double o = 0;
-
-		switch (op) {
-
-		// case to add two numbers
-		case '+':
-
-			o = num1 + num2;
-
-			break;
-
-		// case to subtract two numbers
-		case '-':
-
-			o = num1 - num2;
-
-			break;
-
-		// case to multiply two numbers
-		case '*':
-
-			o = num1 * num2;
-
-			break;
-
-		// case to divide two numbers
-		case '/':
-
-			o = num1 / num2;
-
-			break;
-
-		default:
-
-			System.out.println("You enter wrong input");
-
-			break;
+        switch(select)
+        {
+           case '1' : 
+		System.out.print("Enter Two Numbers : ");
+                a = scan.nextFloat();
+                b = scan.nextFloat();
+                res = a + b;
+                System.out.print("Result = " + res);
+                break;
+           case '2' : 
+		System.out.print("Enter Two Numbers : ");
+                a = scan.nextFloat();
+                b = scan.nextFloat();
+                res = a - b;
+                System.out.print("Result = " + res);
+                break;
+           case '3' : 
+		System.out.print("Enter Two Numbers : ");
+                a = scan.nextFloat();
+                b = scan.nextFloat();
+                res = a * b;
+                System.out.print("Result = " + res);
+                break;
+           case '4' : 
+		System.out.print("Enter Two Numbers : ");
+                a = scan.nextFloat();
+                b = scan.nextFloat();
+                res = a / b;
+                System.out.print("Result = " + res);
+                break;
+	   case '5' : 
+		System.out.print("Enter Value of a : ");
+		double a = scan.nextDouble();
+		System.out.print("Enter Value of b : ");
+		double b = scan.nextDouble();
+		System.out.print("Enter Value of c : ");
+		double c = scan.nextDouble();
+		double d = b * b - 4.0 * a * c;
+		if (d > 0.0)
+		{
+			double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
+			double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
+			System.out.println("The Roots Are " + r1 + " and " + r2;
 		}
-
-		System.out.println("The final result:");
-
-		System.out.println();
-
-		// print the final result
-		System.out.println(num1 + " " + op + " " + num2
-						+ " = " + o);
-	}
+		else if (d == 0.0)
+		{
+			double r1 = -b / (2.0 * a);
+			System.out.println("The Root Is " + r1);
+		}
+		else
+		{
+			System.out.println("Roots Are Not Real");
+		}
+           case '6' : 
+		System.exit(0);
+                break;
+           default : System.out.print("Wrong Input");
+        }
+    }while(choice != 6);       
 }
